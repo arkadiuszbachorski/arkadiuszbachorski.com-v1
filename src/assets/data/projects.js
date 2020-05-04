8999694ff609cbf16a6028e7d02cf7d228053f85
Arkadiusz Bachorski <quez2223@gmail.com>
2020-05-04 22:10:24 +0200

    Provide projects section data

---
+++ src/assets/data/projects.js
@@ -0,0 +1,53 @@
+import { graphql, useStaticQuery } from 'gatsby';
+import { useIntl } from 'gatsby-plugin-intl';
+
+const data = [
+    {
+        id: 'zhonya',
+        name: 'Zhonya',
+        urls: {
+            visit: 'https://www.zhonya.hekko24.pl',
+            repository: null,
+        },
+    },
+    {
+        id: 'xmake',
+        name: 'Xmake',
+        urls: {
+            visit: null,
+            repository: 'https://github.com/arkadiuszbachorski/laravel-xmake',
+        },
+    },
+];
+
+const useProjectImages = () => {
+    const imageData = useStaticQuery(graphql`
+        query {
+            allFile(filter: { absolutePath: { regex: "/projects/" } }) {
+                edges {
+                    node {
+                        childImageSharp {
+                            fluid(maxWidth: 800) {
+                                ...GatsbyImageSharpFluid_noBase64
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    `);
+
+    return imageData.allFile.edges.map((edge) => edge.node.childImageSharp);
+};
+
+const useProjectsData = () => {
+    const images = useProjectImages();
+    const intl = useIntl();
+    return data.map((project) => ({
+        ...project,
+        description: intl.formatMessage({ id: `projects.${project.id}.description` }),
+        image: images.find((image) => image.fluid.src.includes(project.id)),
+    }));
+};
+
+export default useProjectsData;
