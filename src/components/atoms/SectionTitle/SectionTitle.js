416ce4cc5593899d39b9b44082b5fe51989f012d
Arkadiusz Bachorski <quez2223@gmail.com>
2020-05-06 16:53:12 +0200

    Remove accent from SectionTitle

--- src/components/atoms/SectionTitle/SectionTitle.js
+++ src/components/atoms/SectionTitle/SectionTitle.js
@@ -1,5 +1,4 @@
 import styled from 'styled-components';
-import backgroundImage from '../../../assets/images/accent.svg';
 
 const SectionTitle = styled.h2`
     width: 100%;
@@ -7,19 +6,6 @@ const SectionTitle = styled.h2`
     font-weight: 400;
     margin-bottom: 2rem;
     font-size: ${(props) => props.theme.font.size.xl};
-
-    &::before,
-    &::after {
-        content: url(${backgroundImage});
-    }
-
-    &::before {
-        margin-right: 2rem;
-    }
-
-    &::after {
-        margin-left: 2rem;
-    }
 
     @media (${(props) => props.theme.mediaQuery.tablet}) {
         margin-bottom: 3.5rem;
